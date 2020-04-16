e334aa71ada9bf277da779ea7230cf66b07e69c7
Floozutter <floozutter@gmail.com>
2020-04-16 09:47:06 -0700

    Empty strings are falsy

--- misc/deblank.py
+++ misc/deblank.py
@@ -4,6 +4,6 @@ A recursive function that removes spaces from a string, for Rayan.
 
 def deblank(s: str) -> str:
     """Returns the string argument without any spaces."""
-    if s == "":       return ""
+    if not s:         return ""
     elif s[0] == " ": return deblank(s[1:])
     else:             return s[0] + deblank(s[1:])
