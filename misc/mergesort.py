a443ddad704d0d6ca9d13cf9ea496522c4202fe6
Floozutter <floozutter@gmail.com>
2020-05-06 00:20:04 -0700

    Bind TypeVar with a Comparable type

--- misc/mergesort.py
+++ misc/mergesort.py
@@ -1,20 +1,26 @@
 """
-A recursive implementation of mergesort, for Ryan.
+A recursive implementation of mergesort, for Ryan (not Rayan).
 """
 
-from typing import TypeVar, List
+from abc import ABCMeta, abstractmethod
+from typing import TypeVar, Any, List
 
-T = TypeVar("T")
+
+class Comparable(metaclass=ABCMeta):
+    @abstractmethod
+    def __lt__(self, other: Any) -> bool: ...
+
+C = TypeVar("C", bound=Comparable)
 
 
-def merge(a: List[T], b: List[T]) -> List[T]:
+def merge(a: List[C], b: List[C]) -> List[C]:
     """Merges two lists that are sorted in ascending order."""
     if not a:       return b
     if not b:       return a
     if a[0] < b[0]: return a[0:1] + merge(a[1:], b)
     else:           return b[0:1] + merge(a, b[1:])
 
-def mergesort(lst: List[T]) -> List[T]:
+def mergesort(lst: List[C]) -> List[C]:
     """Returns the list sorted using mergesort."""
     s = len(lst)  # size
     if s < 2: return lst
@@ -24,7 +30,7 @@ def mergesort(lst: List[T]) -> List[T]:
 if __name__ == "__main__":
     from functools import partial
     from typing import Callable
-    def check_sort(sort: Callable[[List[T]], List[T]], lst: List[T]) -> bool:
+    def check_sort(sort: Callable[[List[C]], List[C]], lst: List[C]) -> bool:
         """Compares the sorting function against Python's sorted."""
         return sorted(lst) == sort(lst)
     check_mergesort = partial(check_sort, mergesort)
