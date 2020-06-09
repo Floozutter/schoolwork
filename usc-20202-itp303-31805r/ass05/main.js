ef4b3aee2d5efc740f49358e6b68045baf0db3f4
Floozutter <floozutter@gmail.com>
2020-06-08 20:32:23 -0700

    Add function to append data to #results .row

---
+++ usc-20202-itp303-31805r/ass05/main.js
@@ -0,0 +1,30 @@
+function addResult(title, date, imgsrc) {
+	// Create column.
+	let col = document.createElement("div");
+	col.classList.add("col-6", "col-sm-4", "col-md-3", "text-center");
+	// Create and append image.
+	let img = document.createElement("img");
+	img.classList.add("img-fluid", "rounded");
+	img.setAttribute("src", imgsrc);
+	img.setAttribute("alt", title);
+	col.appendChild(img);
+	// Create and append title.
+	let titleElement = document.createElement("h2");
+	titleElement.textContent = title;
+	col.appendChild(titleElement);
+	// Create and append date.
+	let dateElement = document.createElement("h3");
+	dateElement.textContent = date;
+	col.appendChild(dateElement);
+	// Append column to row.
+	let row = document.querySelector("#results .row");
+	row.appendChild(col);
+}
+
+function startup() {
+	for (let i = 0; i < 5; i++) {
+		addResult("GLaDOS", "????-??-??", "https://upload.wikimedia.org/wikipedia/en/b/bf/Glados.png");
+	}
+}
+
+window.addEventListener("load", startup);
