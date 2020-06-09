1b56361ef924b735766fdcc2cd6ef20227ad1ca1
Floozutter <floozutter@gmail.com>
2020-06-08 22:09:49 -0700

    Add request code

--- usc-20202-itp303-31805r/ass05/main.js
+++ usc-20202-itp303-31805r/ass05/main.js
@@ -1,29 +1,56 @@
+function ajax(callback, url) {
+	const req = new XMLHttpRequest();
+	req.onreadystatechange = function() {
+		if (this.readyState == 4) {
+			if (this.status == 200) {
+				callback(this.responseText);
+			} else {
+				console.error("XHR error!");
+			}
+		}
+	}
+	req.open("GET", url);
+	req.send();
+}
+
 function addResult(title, date, imgsrc) {
 	// Create column.
-	let col = document.createElement("div");
+	const col = document.createElement("div");
 	col.classList.add("col-6", "col-sm-4", "col-md-3", "text-center");
 	// Create and append image.
-	let img = document.createElement("img");
+	const img = document.createElement("img");
 	img.classList.add("img-fluid", "rounded");
 	img.setAttribute("src", imgsrc);
 	img.setAttribute("alt", title);
 	col.appendChild(img);
 	// Create and append title.
-	let titleElement = document.createElement("h2");
+	const titleElement = document.createElement("h2");
 	titleElement.textContent = title;
 	col.appendChild(titleElement);
 	// Create and append date.
-	let dateElement = document.createElement("h3");
+	const dateElement = document.createElement("h3");
 	dateElement.textContent = date;
 	col.appendChild(dateElement);
 	// Append column to row.
-	let row = document.querySelector("#results .row");
+	const row = document.querySelector("#results .row");
 	row.appendChild(col);
 }
 
 function clearResults() {
-	let row = document.querySelector("#results .row");
+	const row = document.querySelector("#results .row");
 	while (row.firstChild) { row.removeChild(row.lastChild); }
+}
+
+function displayResults(response) {
+	const data = JSON.parse(response);
+	clearResults();
+	data.results.forEach(function (result) {
+		addResult(
+			result.title,
+			result.release_data,
+			`https://image.tmdb.org/t/p/w300${result.poster_path}`
+		);
+	});
 }
 
 function startup() {
@@ -31,13 +58,7 @@ function startup() {
 		console.error("Failed to load TMDb API key!");
 	}
 	
-	for (let i = 0; i < 2; i++) {
-		addResult("GLaDOS", "????-??-??", "https://upload.wikimedia.org/wikipedia/en/b/bf/Glados.png");
-	}
-	clearResults();
-	for (let i = 0; i < 5; i++) {
-		addResult("GLaDOS", "????-??-??", "https://upload.wikimedia.org/wikipedia/en/b/bf/Glados.png");
-	}
+	ajax(displayResults, `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1&api_key=${APIKEY}`);
 }
 
 window.addEventListener("load", startup);
