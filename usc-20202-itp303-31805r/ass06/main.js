6555c371c23a2034c7b99559d770933ab1bc67d1
Floozutter <floozutter@gmail.com>
2020-06-13 00:00:36 -0700

    Finish ITP303 - Assignment #6

--- usc-20202-itp303-31805r/ass06/main.js
+++ usc-20202-itp303-31805r/ass06/main.js
@@ -1,35 +1,47 @@
-function setWeather(data) {
-	const obs = data.data[0];  // First returned observation.
-	$("#weather-report").text(
-		`${obs.temp}\u2109, feels like ${obs.app_temp}\u2109`
-	);
+function setWeather(city) {
+	console.log(city);
+	$.get(
+		`https://api.weatherbit.io/v2.0/current?city=${city}&units=I&key=${APIKEY}`,
+		function(data) {
+			const obs = data.data[0];  // First returned observation.
+			$("#weather-report").text(
+				`${obs.temp}\u00B0F, feels like ${obs.app_temp}\u00B0F`
+			);
+		}
+	)
 }
-
 function addItem(text) {
 	const item = $("<li></li>");
 	item.append(
 		$("<span></span>")
 			.addClass("close")
-			.text("X")
+			.text("\u2B1C")
 			.click(function() {
-				$(this).closest("li").remove();
+				$(this).closest("li").fadeOut(200, function() {
+					$(this).remove();
+				});
 			})
 	);
-	item.append($("<span></span>").text(" - " + text));
+	item.click(function() {
+		$(this).toggleClass("crossed");
+	});
+	item.append($("<span></span>").text(" " + text));
 	$("#todo ul").append(item);
 }
 
 function startup() {
-	// Get weather from API.
-	$.get(
-		`https://api.weatherbit.io/v2.0/current?city=Los Angeles,CA&units=I&key=${APIKEY}`,
-		setWeather
-	);
-	
-	// Add listener to To-Do List button.
-	$("#todo-button").click(function() {
-		addItem($("#todo-field").val());
+	// Add listeners.
+	const addingListener = function() { addItem($("#todo-field").val()); }
+	$("#todo-button").click(addingListener);
+	$("#todo-field").on("keypress", function(e) {
+		if (e.which === 13) { addingListener(); }
 	});
+	$("#city").change(function() {
+		setWeather($(this).options[$(this).selectedIndex].value);
+	});
+	
+	// Set weather using default city selection.
+	setWeather("Los Angeles,CA");
 	
 	// Add example To-Do List items.
 	addItem("Study for ITP 303 midterm");
