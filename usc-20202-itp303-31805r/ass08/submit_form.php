596fa4c1b4ce8f1c933398fb170b3fae963b0182
Floozutter <floozutter@gmail.com>
2020-06-22 00:06:45 -0700

    Start ITP303 - Assignment #8

---
+++ usc-20202-itp303-31805r/ass08/submit_form.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Confirmation</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
+
+	<div class="container">
+		<div class="row">
+			<h1 class="col-12 mt-5 mb-3">Order Confirmation</h1>
+		</div> <!-- .row -->
+	</div> <!-- .container -->
+	
+	<div class="container">
+
+		<div class="row mt-3">
+			<div>
+				<!-- Change this to date/time that this was submitted. -->
+				This form was submitted on [weekday], [month] [day], [year] at [time].
+			</div>
+		</div>
+
+		<div class="row mt-4">
+			<div class="col-4 text-right">Full Name:</div>
+			<div class="col-8">
+				<!-- PHP Output Here -->
+				
+			</div>
+		</div> <!-- .row -->
+
+		<div class="row mt-3">
+			<div class="col-4 text-right">Phone Number Match:</div>
+			<div class="col-8">
+				<!-- PHP Output Here -->
+				
+			</div>
+		</div> <!-- .row -->
+
+		<div class="row mt-3">
+			<div class="col-4 text-right">Order:</div>
+			<div class="col-8">
+				<!-- PHP Output Here -->
+				
+			</div>
+		</div> <!-- .row -->
+		<div class="row mt-3">
+			<div class="col-4 text-right">Size:</div>
+			<div class="col-8">
+				<!-- PHP Output Here -->
+				
+			</div>
+		</div> <!-- .row -->
+
+		<div class="row mt-3">
+			<div class="col-4 text-right">Flavor shot(s): </div>
+			<div class="col-8">
+				<!-- PHP Output Here -->
+				
+			</div>
+		</div> <!-- .row -->
+
+		<div class="row mt-4 mb-4">
+			<a href="form.php" role="button" class="btn btn-primary">Back to Form</a>
+		</div> <!-- .row -->
+
+	</div> <!-- .container -->
+
+</body>
+</html>
