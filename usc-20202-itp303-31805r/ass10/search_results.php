b7b053abf61e80bd6121bb611e8c9d67e0a06da4
Floozutter <floozutter@gmail.com>
2020-06-26 20:18:48 -0700

    Finish search_results.php

--- usc-20202-itp303-31805r/ass10/search_results.php
+++ usc-20202-itp303-31805r/ass10/search_results.php
@@ -1,7 +1,94 @@
 <?php
+	function panic($errtype, $errno, $errmsg) {
+		echo 'OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!', '</br>';
+		echo "$errtype $errno: $errmsg";
+		exit;
+	}
+	
+	function exists_GET($key) {
+		return isset($_GET[$key]) && !empty($_GET[$key]);
+	}
+	
+	function print_results($mysqli_result) {
+		while ($row = $mysqli_result->fetch_assoc()) {
+			echo '<tr>';
+			echo 	'<td><a href="./details.php?dvd_title_id=' . $row['dvd_title_id'] . '">' . $row['title'] . '</a></td>';
+			echo 	'<td>' . $row['release_date'] . '</td>';
+			echo 	'<td>' . $row['genre'] . '</td>';
+			echo 	'<td>' . $row['rating'] . '</td>';
+			echo '</tr>';
+		}
+	}
+	
+	// Connect to database.
+	$mysqli = new mysqli(
+		'303.itpwebdev.com',
+		'dchoi933_uwu',
+		'0x4BE71AF2459CF83899EC9DC2CB60E22AC4B3047E0211034BBABE9D174C069DD6',
+		'dchoi933_dvd_db'
+	);
+	if ($mysqli->connect_errno) { panic('MySQL Connection Error', $mysqli->connect_errno, $mysqli->connect_error); }
 
-
-
+	// Build SQL query.
+	$query = (
+		' SELECT' .
+		' 	dvd_titles.dvd_title_id,' .
+		' 	dvd_titles.title,' .
+		' 	dvd_titles.release_date,' .
+		' 	genres.genre,' .
+		' 	ratings.rating' .
+		' FROM' .
+		' 	dvd_titles' .
+		' 	LEFT JOIN genres' .
+		' 		ON dvd_titles.genre_id = genres.genre_id' .
+		' 	LEFT JOIN ratings' .
+		' 		ON dvd_titles.rating_id = ratings.rating_id' .
+		' 	LEFT JOIN labels' .
+		' 		ON dvd_titles.label_id = labels.label_id' .
+		' 	LEFT JOIN formats' .
+		' 		ON dvd_titles.format_id = formats.format_id' .
+		' 	LEFT JOIN sounds' .
+		' 		ON dvd_titles.sound_id = sounds.sound_id' .
+		' WHERE 1 = 1'
+	);
+	// Example GET data:
+	// ?title=alien&genre=19&rating=7&label=124&format=8&sound=3&award=any&release_date_from=2010-01-01&release_date_to=2020-06-26
+	if (exists_GET('title')) {
+		$query = $query . " AND (dvd_titles.title LIKE '%{$_GET['title']}%')";
+	}
+	if (exists_GET('genre')) {
+		$query = $query . " AND (dvd_titles.genre_id = {$_GET['genre']})";
+	}
+	if (exists_GET('rating')) {
+		$query = $query . " AND (dvd_titles.rating_id = {$_GET['rating']})";
+	}
+	if (exists_GET('label')) {
+		$query = $query . " AND (dvd_titles.label_id = {$_GET['label']})";
+	}
+	if (exists_GET('format')) {
+		$query = $query . " AND (dvd_titles.format_id = {$_GET['format']})";
+	}
+	if (exists_GET('sound')) {
+		$query = $query . " AND (dvd_titles.sound_id = {$_GET['sound']})";
+	}
+	if (exists_GET('award')) {
+		if ($_GET['award'] == 'yes') {
+			$query = $query . " AND (dvd_titles.award IS NOT NULL)";
+		} elseif ($_GET['award'] == 'no') {
+			$query = $query . " AND (dvd_titles.award IS NULL)";
+		}
+	}
+	if (exists_GET('release_date_from') && exists_GET('release_date_to')) {
+		$query = $query . " AND (dvd_titles.release_date BETWEEN '{$_GET['release_date_from']}' AND '{$_GET['release_date_to']}')";
+	}
+	$query = $query . ';';
+	
+	// Get results.
+	$results = $mysqli->query($query);
+	if (!$results) { panic('MySQL Query Error', $mysqli->errno, $mysqli->error); }
+	
+	// Close connection.
+	$mysqli->close();
 ?>
 <!DOCTYPE html>
 <html>
@@ -29,9 +116,7 @@
 		</div> <!-- .row -->
 		<div class="row">
 			<div class="col-12">
-
-				Showing 0 result(s).
-
+				Showing <?=$results->num_rows?> result(s).
 			</div> <!-- .col -->
 			<div class="col-12">
 				<table class="table table-hover table-responsive mt-4">
@@ -44,17 +129,8 @@
 						</tr>
 					</thead>
 					<tbody>
-
-						<tr>
-							<td>
-								Title name.
-							</td>
-							<td>Release Date</td>
-							<td>Genre</td>
-							<td>Rating</td>
-						</tr>
-
-
+						<?php print_results($results); ?>
+						
 					</tbody>
 				</table>
 			</div> <!-- .col -->
