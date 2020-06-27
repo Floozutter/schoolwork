5ef08b804790cd1a09efe59c48e7b52184a43ba1
Floozutter <floozutter@gmail.com>
2020-06-26 18:30:31 -0700

    Finish search_form.php

--- usc-20202-itp303-31805r/ass10/search_form.php
+++ usc-20202-itp303-31805r/ass10/search_form.php
@@ -1,6 +1,49 @@
 <?php
+	function panic($errtype, $errno, $errmsg) {
+		echo 'OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!', '</br>';
+		echo "$errtype $errno: $errmsg";
+		exit;
+	}
+	
+	function print_options($mysqli_result, $val_key, $text_key) {
+		while ($row = $mysqli_result->fetch_assoc()) {
+			echo '<option value="' . $row[$val_key] . '">';
+			echo $row[$text_key];
+			echo '</option>';
+		}
+	}		
 
+	// Connect to database.
+	$mysqli = new mysqli(
+		'303.itpwebdev.com',
+		'dchoi933_uwu',
+		'0x4BE71AF2459CF83899EC9DC2CB60E22AC4B3047E0211034BBABE9D174C069DD6',
+		'dchoi933_dvd_db'
+	);
+	if ($mysqli->connect_errno) { panic('MySQL Connection Error', $mysqli->connect_errno, $mysqli->connect_error); }
 
+	// Query genres.
+	$genres = $mysqli->query('SELECT * FROM genres');
+	if (!$genres) { panic('MySQL Query Error', $mysqli->errno, $mysqli->error); }
+	
+	// Query ratings.
+	$ratings = $mysqli->query('SELECT * FROM ratings');
+	if (!$ratings) { panic('MySQL Query Error', $mysqli->errno, $mysqli->error); }
+	
+	// Query labels.
+	$labels = $mysqli->query('SELECT * FROM labels');
+	if (!$labels) { panic('MySQL Query Error', $mysqli->errno, $mysqli->error); }
+	
+	// Query formats.
+	$formats = $mysqli->query('SELECT * FROM formats');
+	if (!$formats) { panic('MySQL Query Error', $mysqli->errno, $mysqli->error); }
+	
+	// Query sounds.
+	$sounds = $mysqli->query('SELECT * FROM sounds');
+	if (!$sounds) { panic('MySQL Query Error', $mysqli->errno, $mysqli->error); }
+	
+	// Close connection.
+	$mysqli->close();
 ?>
 <!DOCTYPE html>
 <html>
@@ -39,8 +82,8 @@
 				<div class="col-sm-9">
 					<select name="genre" id="genre-id" class="form-control">
 						<option value="" selected>-- All --</option>
-
 						<!-- Genre dropdown options here -->
+						<?php print_options($genres, 'genre_id', 'genre'); ?>
 
 					</select>
 				</div>
@@ -50,8 +93,8 @@
 				<div class="col-sm-9">
 					<select name="rating" id="rating-id" class="form-control">
 						<option value="" selected>-- All --</option>
-
 						<!-- Rating dropdown options here -->
+						<?php print_options($ratings, 'rating_id', 'rating'); ?>
 
 					</select>
 				</div>
@@ -61,8 +104,8 @@
 				<div class="col-sm-9">
 					<select name="label" id="label-id" class="form-control">
 						<option value="" selected>-- All --</option>
-
 						<!-- Label dropdown options here -->
+						<?php print_options($labels, 'label_id', 'label'); ?>
 
 					</select>
 				</div>
@@ -72,8 +115,8 @@
 				<div class="col-sm-9">
 					<select name="format" id="format-id" class="form-control">
 						<option value="" selected>-- All --</option>
-
 						<!-- Format dropdown options here -->
+						<?php print_options($formats, 'format_id', 'format'); ?>
 
 					</select>
 				</div>
@@ -83,9 +126,8 @@
 				<div class="col-sm-9">
 					<select name="sound" id="sound-id" class="form-control">
 						<option value="" selected>-- All --</option>
-
 						<!-- Sound dropdown options here -->
-						
+						<?php print_options($sounds, 'sound_id', 'sound'); ?>
 
 					</select>
 				</div>
