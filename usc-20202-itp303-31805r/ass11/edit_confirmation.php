34aceba7580bea0127bd3bbfa06294b5bc52514d
Floozutter <floozutter@gmail.com>
2020-06-29 22:52:03 -0700

    Finish edit_confirmation.php

--- usc-20202-itp303-31805r/ass11/edit_confirmation.php
+++ usc-20202-itp303-31805r/ass11/edit_confirmation.php
@@ -1,3 +1,78 @@
+<?php
+	function missing_POST($key) {
+		return !isset($_POST[$key]) || empty($_POST[$key]);
+	}
+	function null_fallback_POST($key) {
+		return (
+			missing_POST($key)
+			? 'NULL'
+			: $_POST[$key]
+		);
+	}
+	function null_fallback_quoted_POST($key) {
+		return (
+			missing_POST($key)
+			? 'NULL'
+			: '\'' . $_POST[$key] . '\''
+		);
+	}
+	
+	function attempt() {
+		// Guarantee dvd_title_id and title exists.
+		if (missing_POST('dvd_title_id')) {
+			return '<div class="text-danger font-italic">Missing dvd_title_id!</div>';
+		}
+		if (missing_POST('title')) {
+			return '<div class="text-danger font-italic">Missing title!</div>';
+		}
+		
+		// Connect to database.
+		$mysqli = new mysqli(
+			'303.itpwebdev.com',
+			'dchoi933_uwu',
+			'0x4BE71AF2459CF83899EC9DC2CB60E22AC4B3047E0211034BBABE9D174C069DD6',
+			'dchoi933_dvd_db'
+		);
+		if ($mysqli->connect_errno) {
+			return '<div class="text-danger font-italic">Connect error!</div>';
+		}
+		
+		// Get POST values.
+		$dvd_title_id = $_POST['dvd_title_id'];
+		$title = null_fallback_quoted_POST('title');
+		$release_date = null_fallback_quoted_POST('release_date');
+		$award = null_fallback_quoted_POST('award');
+		$label_id = null_fallback_POST('label_id');
+		$sound_id = null_fallback_POST('sound_id');
+		$genre_id = null_fallback_POST('genre_id');
+		$rating_id = null_fallback_POST('rating_id');
+		$format_id = null_fallback_POST('format_id');
+		
+		// Query.
+		$query = (
+			' UPDATE dvd_titles' .
+			' SET' . 
+			" 	title = $title," .
+			" 	release_date = $release_date," .
+			" 	award = $award," .
+			" 	label_id = $label_id," .
+			" 	sound_id = $sound_id," .
+			" 	genre_id = $genre_id," .
+			" 	rating_id = $rating_id," .
+			" 	format_id = $format_id" .
+			" WHERE dvd_title_id = $dvd_title_id;"
+		);
+		$results = $mysqli->query($query);
+		if (!$results) {
+			return '<div class="text-danger font-italic">Query error! ' . $query . '</div>';
+		}
+		
+		// Return success message.
+		return '<div class="text-success"><span class="font-italic">' . $title . '</span> was successfully edited.</div>';
+	}
+	
+	$msg = attempt();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -23,16 +98,13 @@
 	<div class="container">
 		<div class="row mt-4">
 			<div class="col-12">
-
-				<div class="text-danger font-italic">Display Error Messages Here</div>
-
-				<div class="text-success"><span class="font-italic">Title</span> was successfully edited.</div>
+				<?=$msg?>
 
 			</div> <!-- .col -->
 		</div> <!-- .row -->
 		<div class="row mt-4 mb-4">
 			<div class="col-12">
-				<a href="details.php" role="button" class="btn btn-primary">Back to Details</a>
+				<a href="details.php?dvd_title_id=<?=$_POST['dvd_title_id']?>" role="button" class="btn btn-primary">Back to Details</a>
 			</div> <!-- .col -->
 		</div> <!-- .row -->
 	</div> <!-- .container -->
