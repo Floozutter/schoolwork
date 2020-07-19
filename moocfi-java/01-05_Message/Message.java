144ceed0d61cbea00c2eccfa52cd86ba16fc741f
Floozutter <floozutter@gmail.com>
2020-07-19 01:19:54 -0700

    Add 01-05_Message

---
+++ moocfi-java/01-05_Message/Message.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Message {
+	public static void main(String[] args) {
+		System.out.println("Write a message:");
+		System.out.println(
+			(new Scanner(System.in)).nextLine()
+		);
+	}
+}
