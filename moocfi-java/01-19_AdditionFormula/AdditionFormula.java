d3a1986aaab4e564719fd142eeb17c9c87a31bc5
Floozutter <floozutter@gmail.com>
2020-08-17 00:07:22 -0700

    Add 01-19_AdditionFormula

---
+++ moocfi-java/01-19_AdditionFormula/AdditionFormula.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class AdditionFormula {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Give the first number:");
+		final int first = Integer.valueOf(scanner.nextLine());
+		
+		System.out.println("Give the second number:");
+		final int second = Integer.valueOf(scanner.nextLine());
+		
+		System.out.println(String.format(
+			"%d + %d = %d",
+			first, second, first + second
+		));
+	}
+}
