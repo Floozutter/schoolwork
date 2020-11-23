3a1c9c5313467422ab706c4b49f6d8100a8dee4e
Floozutter <floozutter@gmail.com>
2020-11-22 18:34:17 -0800

    Clean style

--- ecc-20203-csci146+147/database.cpp
+++ ecc-20203-csci146+147/database.cpp
@@ -1,49 +1,35 @@
-const int MAX_MARTIANS = 100;
+#include <iostream>
+#include <fstream>
+#include <istream>
+#include <string>
+
+int const MAX_MARTIANS = 100;
 
 struct Martian {
-    std::string fname_;
-    std::string lname_;
-    std::string SSN_;
-    Martian(std::string fn, std::string ln, std::string ssn)
-        : fname_(fn), lname_(ln), SSN_(ssn) { }
-
-    // you may add other things to the structure	
+	std::string fname;
+	std::string lname;
+	std::string ssn;
 };
 
-enum class Criteria { first, last, ssn };
+enum class Criterion {
+	first,
+	last,
+	ssn
+};
 
 class MartianDatabase {
 public:
-    // creates a new empty Martian Database initialized with the
-    // contents of filename.
-    MartianDatabase(std::istream& infile);
-    
-    // add item m to database, return true if successfully added.
-    bool addMartian(const Martian &m);
-    
-    // delete item with the ssn in database, return true if successfully 
-    // deleted.
-    bool deleteMartian(std::string ssn);
-    
-    // return true if the database does not contain any items, false
-    // otherwise.
-    bool isEmpty();
-    
-    // return true if the database is at its maximum storage, false
-    // otherwise.
-    bool isFull();    
-    
-    // prints the list of Martians, sorted by last name, sorted
-    // by first name and by MSSN.
-    void printMartians(Criteria type);
-
-    // deletes all the dynamically created memory
-    ~MartianDatabase();
+	MartianDatabase(std::istream & is);  // Initialize with input stream.
+	~MartianDatabase();
+	bool addMartian(Martian const & m);  // Returns true if sufficient space.
+	bool deleteMartian(std::string ssn);  // Returns true if found.
+	bool isEmpty();
+	bool isFull();    
+	void printMartians(Criterion crit);  // Prints items sorted by criterion.
 private:
     Martian* m_unsorted[MAX_MARTIANS];
     Martian* m_fname[MAX_MARTIANS];
     Martian* m_lname[MAX_MARTIANS];
     Martian* m_ssn[MAX_MARTIANS];
-
-    // you may add other private members to the class
+	size_t m_size = 0;
 };
