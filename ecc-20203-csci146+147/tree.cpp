c593d2267cecbcc47d907c4b20eeef9f7ddff17e
Floozutter <floozutter@gmail.com>
2020-12-07 19:21:54 -0800

    Finish tree.cpp

--- ecc-20203-csci146+147/tree.cpp
+++ ecc-20203-csci146+147/tree.cpp
@@ -1,39 +1,142 @@
+#include <cassert>
 #include <iostream>
 #include <string>
-using namespace std;
+#include <utility>
 
-typedef string ItemType;
-
-struct WordNode {
-	ItemType m_data;
-	WordNode *m_left;
-	WordNode *m_right;
-	// You may add additional data members and member functions in WordNode.
+template <typename T>
+class Tree {
+public:          
+	Tree();
+	~Tree();
+	Tree(Tree const & other);
+	template<typename U> friend void swap(Tree<U> & a, Tree<U> & b);
+	Tree const & operator=(Tree other);
+	void add(T value);
+	int distinctValues() const;
+	int totalValues() const;
+	template<typename U> friend std::ostream & operator<<(
+		std::ostream & out, Tree<U> const & tree
+	);
+private:
+	struct Node {
+		T value;
+		size_t count;
+		Node * left;
+		Node * right;
+	};
+	static int countValues(Node const * root, bool distinct);
+	Node * root = nullptr;
 };
 
-class WordTree {
-private:
-	WordNode *root;
+template<typename T>
+Tree<T>::Tree() {}
+
+template<typename T>
+Tree<T>::~Tree() {
+	auto const destroyTree = [](auto && self, Node * root) -> void {
+		if (root) {
+			self(self, root->left);
+			self(self, root->right);
+			delete root;
+		}
+	};
+	destroyTree(destroyTree, this->root);
+}
+
+template<typename T>
+Tree<T>::Tree(Tree const & other) {
+	auto const copyTree = [](auto && self, Node const * root) -> Node * {
+		if (root) {
+			Node * newroot{new Node{*root}};
+			newroot->left = self(self, root->left);
+			newroot->right = self(self, root->right);
+			return newroot;
+		} else {
+			return nullptr;
+		}
+	};
+	this->root = copyTree(copyTree, other.root);
+}
+
+template <typename T>
+void swap(Tree<T> & a, Tree<T> & b) {
+	using std::swap;
+	swap(a.root, b.root);
+}
+
+template <typename T>
+Tree<T> const & Tree<T>::operator=(Tree other) {
+	swap(*this, other);
+	return *this;
+}
+
+template <typename T>
+void Tree<T>::add(T value) {
+	auto const addToTree = [](auto && self, Node * root, T value) -> Node * {
+		if (root) {
+			if (value < root->value) {
+				root->left = self(self, root->left, value);
+			} else if (value > root->value) {
+				root->right = self(self, root->right, value);
+			} else {
+				++root->count;
+			}
+			return root;
+		} else {
+			return new Tree::Node{value, 1, nullptr, nullptr};
+		}
+	};
+	this->root = addToTree(addToTree, this->root, value);
+}
+
+template <typename T>
+int Tree<T>::countValues(Node const * root, bool distinct) {
+	if (root) {
+		return (
+			(distinct ? 1 : root->count) +
+			Tree::countValues(root->left, distinct) +
+			Tree::countValues(root->right, distinct)
+		);
+	} else {
+		return 0;
+	}
+}
+
+template <typename T>
+int Tree<T>::distinctValues() const {
+	return Tree::countValues(this->root, true);
+}
+
+template <typename T>
+int Tree<T>::totalValues() const {
+	return Tree::countValues(this->root, false);
+}
+
+template <typename T>
+std::ostream & operator<<(std::ostream & out, Tree<T> const & tree) {
+	auto const printTree = [&out](
+		auto && self,
+		typename Tree<T>::Node const * root
+	) -> void {
+		if (root) {
+			self(self, root->left);
+			out << "(" << root->value << ", " << root->count << ") ";
+			self(self, root->right);
+		}
+	};
+	printTree(printTree, tree.root);
+	out << std::endl;
+	return out;
+}
+
+class WordTree : public Tree<std::string> {
 public:
-	// default constructor            
-	WordTree() : root(nullptr) {}
-	// copy constructor
-	WordTree(const WordTree& rhs);
-	// assignment operator
-	const WordTree& operator=(const WordTree& rhs);
-	// Inserts v into the WordTree.
-	void add(ItemType v);
-	// Returns the number of distince words / nodes.
-	int distinctWords() const;
-	// Returns the total number of words inserted, including duplicate values.
-	int totalWords() const;
-	// Prints the LinkedList.
-	friend ostream& operator<<(ostream &out, const WordTree& rhs);
-	// Destroys all the dynamically allocated memoryin the tree.
-	~WordTree();
+	int distinctWords() const { return this->distinctValues(); }
+	int totalWords() const { return this->totalValues(); }
 };
 
 int main() {
+	// Test WordTree::distinctWords and WordTree::totalWords.
 	WordTree a;
 	a.add("Skyler");
 	a.add("Walter");
@@ -49,5 +152,13 @@ int main() {
 	b.add("Gus");
 	b.add("Walter");
 	b.add("Gus");
-	cout << b;
+	assert(b.distinctWords() == 4);
+	assert(b.totalWords() == 7);
+	// Test copy constructor and copy assignment operator.
+	std::cout << "a: " << a;
+	std::cout << "b: "  << b;
+	WordTree c{b};
+	std::cout << "c (copy constructed from a): "  << c;
+	c = a;
+	std::cout << "c (copy assigned to from b): "  << c;
 }
